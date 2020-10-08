b1c6f62d84549cb9d8dbb9ba9c4ad3a6c464b48c
Leandro Koller <lekoller@yahoo.com>
2020-10-08 16:10:36 -0300

    test: Landing page Cypress test completed so far.

--- cypress/integration/examples/landing.spec.js
+++ cypress/integration/examples/landing.spec.js
@@ -1,5 +1,5 @@
-context('Login and Register Page', () => {
-  it('logs and registers correctly', () => {
+context('Login and Register (Landing) Page Rendering', () => {
+  it('renders landing page and logs with valid credentials', () => {
     cy.visit('http://localhost:3000');
 
     cy.contains('Exemplo');
@@ -12,11 +12,27 @@ context('Login and Register Page', () => {
     cy.contains('Fluxo dinâmico e intuitivo,');
     cy.contains('eficiência elevada ao máximo!');
 
-    // cy.get(':nth-child(3) > .sc-bdnylx').contains('Entrar');
+    cy.get(':nth-child(3) > .sc-bdnylx').contains('Entrar');
     cy.get('.sc-bQCGiA > img');
     cy.get('.sc-hKFyIo').type('leandro@gmail.com');
     cy.get('.sc-jSFkmK').type('123456');
     cy.get('.sc-htmbXw > .sc-bdnylx').contains('Comece agora');
     cy.get(':nth-child(3) > .sc-bdnylx').click();
+  });
+});
+
+context('Modal Register in Landing Page', () => {
+  it('open register modal', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.sc-htmbXw > .sc-bdnylx').click();
+
+    cy.contains('Comece hoje mesmo, a gerenciar seu tempo ou equipe.');
+
+    cy.get('.sc-dvUxUk > :nth-child(1) > .sc-hKFyIo');
+    cy.get(':nth-child(2) > .sc-hKFyIo');
+    cy.get('.sc-dvUxUk > .sc-eCApGN > .sc-jSFkmK');
+    cy.get('.sc-dvUxUk > .sc-bdnylx').click();
+    cy.get('.sc-eWnSVe > img').click();
   });
 });
