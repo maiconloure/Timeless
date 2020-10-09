52b3e61f27ede67b31a03821f4fb08c3820237e4
Leandro Koller <lekoller@yahoo.com>
2020-10-09 11:36:07 -0300

    test: Erase tests for adapt to new behavior.

--- cypress/integration/examples/board.spec.js
+++ cypress/integration/examples/board.spec.js
@@ -9,11 +9,11 @@ context('Logging to Board Page', () => {
     cy.get(':nth-child(3) > .sc-bdnylx').click();
 
     cy.get(':nth-child(2) > .sc-FRqcf > h4').contains('OBJETOS');
-    cy.get('#card').click();
-    cy.get('#fast').click();
     cy.get(':nth-child(3) > .sc-FRqcf > h4').contains('FERRAMENTAS');
     cy.get('.sc-cBoprd > h2').contains('Notificações recentes');
 
-    cy.get(':nth-child(1) > :nth-child(2) > p').contains('aqui vai uma descrição');
+    cy.get('#card').click();
+    cy.get('#card').click();
+    cy.get('#fast').click();
   });
 });
