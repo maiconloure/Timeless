6b2af466a069eb6a5b9d2526b2aeb1f264e22adf
Leandro Koller <lekoller@yahoo.com>
2020-10-12 21:36:05 -0300

    test: Remaking Cypress tests.

--- cypress/integration/examples/board.spec.js
+++ cypress/integration/examples/board.spec.js
@@ -1,19 +1,20 @@
-const { createYield } = require('typescript');
+// const { createYield } = require('typescript');
 
 context('Logging to Board Page', () => {
-  it('logs in and tests full render', () => {
+  it('logs in and tests creating and deleting cards', () => {
+    cy.viewport(1440, 900);
     cy.visit('http://localhost:3000');
 
     cy.get('.sc-hKFyIo').type('leandro@gmail.com');
     cy.get('.sc-jSFkmK').type('123456');
     cy.get(':nth-child(3) > .sc-bdnylx').click();
 
-    cy.get(':nth-child(2) > .sc-FRqcf > h4').contains('OBJETOS');
-    cy.get(':nth-child(3) > .sc-FRqcf > h4').contains('FERRAMENTAS');
+    cy.get(':nth-child(2) > .sc-fbIXFq > h4').contains('OBJETOS');
+    cy.get(':nth-child(3) > .sc-fbIXFq > h4').contains('FERRAMENTAS');
     cy.get('.sc-cBoprd > h2').contains('Notificações recentes');
 
-    cy.get('#card').click();
-    cy.get('#card').click();
-    cy.get('#fast').click();
+    cy.get(':nth-child(2) > .sc-fXazxj > :nth-child(2)').click();
+
+    // cy.get('#fast').click();
   });
 });
