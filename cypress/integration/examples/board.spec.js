c7ed32612f67a643111e89d8b91c6070561b7ff9
Leandro Koller <lekoller@yahoo.com>
2020-10-15 12:43:58 -0300

    feat: Add new Cypress tests to the board.

--- cypress/integration/examples/board.spec.js
+++ cypress/integration/examples/board.spec.js
@@ -1,24 +1,40 @@
 // const { createYield } = require('typescript');
 
 context('Logging to Board Page', () => {
-  it('logs in and tests creating and deleting cards', () => {
+  it('logs in and tests creating cards', () => {
     cy.viewport(1440, 900);
     cy.visit('http://localhost:3000');
-
     cy.get('.sc-hKFyIo').type('leandro@gmail.com');
     cy.get('.sc-jSFkmK').type('123456');
     cy.get(':nth-child(3) > .sc-bdnylx').click();
 
-    // cy.get(':nth-child(2) > .sc-FRqcf > h4').contains('OBJETOS');
-    cy.get(':nth-child(2) > .sc-dvXXZy > h4').contains('OBJETOS');
-    // cy.get(':nth-child(3) > .sc-FRqcf > h4').contains('FERRAMENTAS');
-    cy.get(':nth-child(3) > .sc-dvXXZy > h4').contains('FERRAMENTAS');
+    cy.get(':nth-child(2) > .sc-bsatbK > h4').contains('OBJETOS');
+    cy.get(':nth-child(3) > .sc-bsatbK > h4').contains('FERRAMENTAS');
     cy.get('.sc-cBoprd > h2').contains('Notificações recentes');
 
     cy.wait(2000);
     cy.get('#card').click({ force: true });
-    cy.get('#trash').click();
-    cy.wait(2000);
-    cy.get(':nth-child(6) > .sc-jNnnWF > .sc-cTJmaU > .sc-eirseW').click();
+  });
+
+  it('pins a card', () => {
+    cy.viewport(1440, 900);
+    cy.visit('http://localhost:3000');
+    cy.get('.sc-hKFyIo').type('leandro@gmail.com');
+    cy.get('.sc-jSFkmK').type('123456');
+    cy.get(':nth-child(3) > .sc-bdnylx').click();
+
+    cy.get('#pin').click();
+    cy.get('#card12 > .sc-eirseW').click();
+  });
+
+  it('blocks a card', () => {
+    cy.viewport(1440, 900);
+    cy.visit('http://localhost:3000');
+    cy.get('.sc-hKFyIo').type('leandro@gmail.com');
+    cy.get('.sc-jSFkmK').type('123456');
+    cy.get(':nth-child(3) > .sc-bdnylx').click();
+
+    cy.get('#blocked').click();
+    cy.get('#card11 > .sc-eirseW').click();
   });
 });
--- cypress/integration/examples/landing.spec.js
+++ cypress/integration/examples/landing.spec.js
@@ -40,3 +40,22 @@ context('Modal Register in Landing Page', () => {
     cy.get('.sc-gGGEav > img').click();
   });
 });
+
+context('Login and Register (Landing) Page Rendering', () => {
+  it('Render and click buttons on landing page', () => {
+    cy.viewport(1440, 900);
+    cy.visit('http://localhost:3000');
+
+    cy.contains('Exemplo');
+    cy.contains('Times');
+    cy.contains('Sobre');
+    cy.wait(2000);
+    cy.get('.sc-flUlJl > :nth-child(1)').click();
+    cy.wait(2000);
+    cy.get('.sc-flUlJl > :nth-child(2)').click();
+    cy.wait(2000);
+    cy.get('.sc-flUlJl > :nth-child(3)').click();
+    cy.wait(2000);
+    cy.get('.sc-gkCpWe > img').click();
+  });
+});
