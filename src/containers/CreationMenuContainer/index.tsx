e50bf16700bf3a79f4322f913c157f254a7bd59a
Leandro Koller <lekoller@yahoo.com>
2020-10-15 17:34:26 +0000

    Tests maintenance

--- src/containers/CreationMenuContainer/index.tsx
+++ src/containers/CreationMenuContainer/index.tsx
@@ -167,7 +167,7 @@ const CreationMenuContainer = ({
   };
 
   const pinCardButton = () => {
-    toggleMenu();
+    // toggleMenu();
     setSelectedCard({ ...initialSelectCard, followedCard: !selectedCard.followedCard });
   };
 
