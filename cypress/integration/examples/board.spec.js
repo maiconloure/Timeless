c6d9630297b55fdf23fabe01c955d4b6b38693da
Leandro Koller <lekoller@yahoo.com>
2020-10-16 00:00:29 +0000

    Update all tests

--- cypress/integration/examples/board.spec.js
+++ cypress/integration/examples/board.spec.js
@@ -38,3 +38,38 @@ context('Logging to Board Page', () => {
     cy.get('#card11 > .sc-eirseW').click();
   });
 });
+
+context('Logging to Board Page', () => {
+  it('logs in and tests all creation menu buttons', () => {
+    cy.viewport(1440, 900);
+    cy.visit('http://localhost:3000');
+
+    cy.get('.sc-hKFyIo').type('rafael@gmail.com');
+    cy.wait(1000);
+    cy.get('.sc-jSFkmK').type('123456');
+    cy.wait(1000);
+    cy.get(':nth-child(3) > .sc-bdnylx').click();
+    cy.wait(4000);
+
+    cy.get('#group').click({ force: true });
+    cy.wait(1500);
+    cy.get('#card').click({ force: true });
+    cy.wait(1500);
+    cy.get('#fast').click({ force: true });
+    cy.wait(1500);
+    cy.get('#trash').click({ force: true });
+    cy.wait(1500);
+    cy.get('#addText').click({ force: true });
+    cy.wait(1500);
+    cy.get('#connect').click({ force: true });
+    cy.wait(1500);
+    cy.get('#pin').click({ force: true });
+    cy.wait(1500);
+    cy.get('#blocked').click({ force: true });
+    cy.wait(1500);
+    cy.get('.sc-jtiWoB > img').click();
+    cy.wait(1500);
+    cy.get('.sc-ellfmu > :nth-child(4)').click({ force: true });
+    cy.wait(1500);
+  });
+});
