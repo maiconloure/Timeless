342d7dc33b6fb5f281fdd5f9ce2cf53ead487359
Maicon Lourenço <maiconloure@gmail.com>
2020-10-17 14:02:33 -0300

    update configs

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,12 +2,4 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
-
 declare module 'react-xarrows';
-
-declare namespace React {
-  interface HTMLAttributes<T> extends DOMAttributes<T> {
-    css?: any;
-    style?: any;
-  }
-}
